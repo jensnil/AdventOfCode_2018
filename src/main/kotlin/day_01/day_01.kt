c288dc28c2964a590b077b805032b047095fc1b9
Jens Nilsson <jens.nilsson@leovegas.com>
2018-12-02 01:10:25 +0100

    Day 01

---
+++ src/main/kotlin/day_01/day_01.kt
@@ -0,0 +1,20 @@
+package day_01
+
+import lib.CircularList
+
+fun partOne(input : List<Int>) : Int {
+    return input.sum()
+}
+
+fun partTwo(inputCircular : CircularList<Int>) : Int {
+    var index = 0
+    val usedFrequencies = mutableSetOf<Int>()
+    var latestFrequency = 0
+
+    while (!usedFrequencies.contains(latestFrequency)) {
+        usedFrequencies.add(latestFrequency)
+        latestFrequency += inputCircular[index]
+        index++
+    }
+    return latestFrequency
+}
